0d0307a4e34f6f2575b0d8b5873709244b148d92
YoshuaPariona <loganpariona.1207@gmail.com>
2025-08-05 18:42:39 -0500

    feat: Añadir menú con opciones contextuales

--- src/main/java/com/challenge/literalura/LiteraluraApplication.java
+++ src/main/java/com/challenge/literalura/LiteraluraApplication.java
@@ -1,13 +1,19 @@
 package com.challenge.literalura;
 
+import com.challenge.literalura.main.Main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class LiteraluraApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
+
+		Main main =new Main();
+		main.showMenu();
+
 	}
 
 }
---
+++ src/main/java/com/challenge/literalura/main/Main.java
@@ -0,0 +1,76 @@
+package com.challenge.literalura.main;
+
+import java.util.Scanner;
+
+//Clase principal
+public class Main {
+
+    Scanner userInput = new Scanner(System.in);
+
+    public void showMenu() {
+        String menu = """
+            ======== Bienvenido al menú de Literalura! ========
+    
+            1) Buscar y registrar un libro según su título
+            2) Mostrar libros registrados
+            3) Mostrar autores registrados
+            4) Mostrar autores vivos de un año determinado
+            5) Mostrar libros según su idioma
+            
+            0) Salir
+            
+            Elija una opción para continuar:
+            """;
+        int opc = -1;
+
+        while(opc != 0) {
+            System.out.println(menu);
+            opc = userInput.nextInt();
+
+            switch (opc) {
+                case 1:
+                    showAndRegisterBookByTitle();
+                    break;
+                case 2:
+                    showAllBooks();
+                    break;
+                case 3:
+                    showAllAuthors();
+                    break;
+                case 4:
+                    showAuthorsInYear();
+                    break;
+                case 5:
+                    showBooksByLanguage();
+                    break;
+                case 0:
+                    System.out.println("Cerrando la aplicación . . .");
+                    break;
+                default:
+                    System.out.println("Error... Ingrese una opción válida");
+                    break;
+            }
+
+        }
+    }
+
+    private void showBooksByLanguage() {
+        System.out.println("En desarrollo");
+    }
+
+    private void showAuthorsInYear() {
+        System.out.println("En desarrollo");
+    }
+
+    private void showAllAuthors() {
+        System.out.println("En desarrollo");
+    }
+
+    private void showAllBooks() {
+        System.out.println("En desarrollo");
+    }
+
+    private void showAndRegisterBookByTitle() {
+        System.out.println("En desarrollo");
+    }
+}
